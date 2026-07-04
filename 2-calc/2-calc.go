afd317a3a6c6e6c6e764ee5bb6b4d7428ddfb509
brave0duck <brave0duck@gmail.com>
2026-07-04 23:04:33 +0900

    =

--- 2-calc/2-calc.go
+++ 2-calc/2-calc.go
@@ -1,31 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strconv"
 	"strings"
 )
 
 func main() {
 	var input string
-	fmt.Println("Mini Calc ( a + b ) : ")
-	fmt.Scanf("%s\n", &input)
 
-	str := strings.Split(input, " ")
-	a,err := strconv.Atoi(str[0])
-	if err != nil{
-		fmt.Println("failed convert string to int")
-		return
-	}
-	b, err := strconv.Atoi(str[2])
-	if err != nil{
-		fmt.Println("failed convert string to int")
-		return
-	}
-	if result := calc(a, b, str[1]); result != -1 {
-		fmt.Println("result : ", result)
-	} else {
-		fmt.Println("Can't calc")
+	fmt.Println("Mini Calc (with spaces. a + b ) : ")
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		input = scanner.Text()
+
+		str := strings.Split(input, " ")
+		a, err := strconv.Atoi(str[0])
+		if err != nil {
+			fmt.Println("failed convert string")
+			return
+		}
+		b, err := strconv.Atoi(str[2])
+		if err != nil {
+			fmt.Println("failed convert string")
+			return
+		}
+		if result := calc(a, b, str[1]); result != -1 {
+			fmt.Println("result : ", result)
+		} else {
+			fmt.Println("Can't calc")
+		}
 	}
 
 }
--- 3-bmi/3-bmi.go
+++ 3-bmi/3-bmi.go
@@ -1,5 +1,10 @@
-// bmi = 키/체중의 제곱
-// 저체중: 18.5 미만정상: 18.5 ~ 22.9비만전단계(과체중): 23.0 ~ 24.91단계 비만: 25.0 ~ 29.92단계 비만: 30.0 ~ 34.93단계 비만: 35.0 이상
+// bmi계산 프로그램
+// 저체중: 18.5 미만
+// 정상: 18.5 ~ 22.9
+// 비만전단계(과체중): 23.0 ~ 24.91단계
+// 비만: 25.0 ~ 29.92단계
+// 비만: 30.0 ~ 34.93단계
+// 비만: 35.0 이상
 package main
 
 import "fmt"
@@ -9,31 +14,43 @@ func main() {
 	var weight int
 
 	fmt.Println("당신의 BMI를 체크합니다.")
-	fmt.Println("키 (cm) : ")
+	fmt.Print("키 (cm) : ")
 	fmt.Scanf("%d\n", &height)
-	fmt.Println("체중 (kg) : ")
+	fmt.Print("체중 (kg) : ")
 	fmt.Scanf("%d\n", &weight)
-
+램
 	s := bmi_calc(height, weight)
-	if s != "" {
+	if s == "" {
 		fmt.Println("error occured")
+	} else {
+		fmt.Println(s)
 	}
+}
+func double_weight(n int) float64 {
+	w := float64(n) / 100
+	return w * w
 }
 func bmi_calc(height int, weight int) string {
 	if height < 0 || weight < 0 {
 		fmt.Println("wrong number under 0")
 		return ""
 	}
-	bmi := float64(height / weight * weight)
+	var message string
+	bmi := float64(weight) / double_weight(height)
+
 	if bmi < 18.5 {
-		return fmt.Sprintf("%.2f. 저체중입니다.", bmi)
+		message = "[ 저체중 ] 입니다."
 	} else if bmi < 22.9 {
-		return fmt.Sprintf("%.2f. 과체중입니다.", bmi)
-	} else if bmi < 24.9 {
-		return fmt.Sprintf("%.2f. 1단계 비만입니다.", bmi)
+		message = "[ 정상 ] 입니다."
+	} else if bmi < 25 {
+		message = "[ 과체중 ] 입니다."
 	} else if bmi < 29.2 {
-		return fmt.Sprintf("%.2f. 2단계 비만입니다.", bmi)
+		message = "[ 1단계 비만 ] 입니다."
+	} else if bmi < 34.9 {
+		message = "[ 2단계 비만 ] 입니다."
 	} else {
-		return fmt.Sprintf("%.2f. 비만입니다", bmi)
+		message = "[ 3단계 비만 ] 입니다."
 	}
+	return fmt.Sprintf("당신의 BMI는 %.2f이며 %s 정상체중은 %.2f - %.2f kg 입니다",
+		bmi, message, 18.5*double_weight(height), 22.9*double_weight(height))
 }
