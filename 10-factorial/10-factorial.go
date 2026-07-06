a48bda1f04068c44630d361d94da18509821ea6a
brave0duck <brave0duck@gmail.com>
2026-07-06 20:20:32 +0900

    팩토리얼

--- 10-factorial/10-factorial.go
+++ 10-factorial/10-factorial.go
@@ -2,29 +2,40 @@ package main
 
 import (
 	"fmt"
+	"math/big"
+	"os"
+	"strconv"
 )
-
+// 재귀방식
 func fac_v1(n int) int {
 	if n <= 1 {
 		return n
 	}
-	fmt.Println(n)
 	return n * fac_v1(n-1)
 }
-func fac_v2(n int) int {
-	var result int = 1
+
+// int64이상의 숫자도 가능한 팩토리얼 함수. math.big사용
+func fac_v2(n int) *big.Int {
+	f := big.NewInt(1)
+
 	for i := n; i > 0; i-- {
-		result *= i
-		fmt.Println(result)
+		f.Mul(f, big.NewInt(int64(i)))
 	}
-	return result
+	return f
 }
 func main() {
-	var input int
-	fmt.Println("팩토리얼 계산기")
-	fmt.Print("입력 : ")
-	fmt.Scanf("%d\n", &input)
+	if len(os.Args) < 2 {
+		fmt.Println("사용법: factorial <숫자>")
+		return
+	}
+
+	input, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println("사용법: factorial <숫자>")
+		return
+	}
 
 	fmt.Println("재귀 방식 : ", fac_v1(input))
 	fmt.Println("for문 방식 : ", fac_v2(input))
+
 }
