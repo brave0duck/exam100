de1ffb067cf80377497fbf3f3ec95c60a874339d
brave0duck <brave0duck@gmail.com>
2026-07-07 20:08:43 +0900

    --

---
+++ 14-star/14-star.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func starPrint(n int) {
+	s := strings.Repeat("* ", n)
+
+	for i := 0; i < n; i++ {
+		fmt.Println(s)
+	}
+}
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage : star <num>")
+	}
+	n, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println("Can't convert string")
+	}
+	starPrint(n)
+
+}
---
+++ 14-star/go.mod
@@ -0,0 +1,3 @@
+module 14-star
+
+go 1.25.0
