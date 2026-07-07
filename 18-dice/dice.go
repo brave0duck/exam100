a95d0939d9dcd381b1986c41a6c0c73170dcafcb
brave0duck <brave0duck@gmail.com>
2026-07-07 18:24:06 +0900

    dice

--- 18-dice/dice.go
+++ 18-dice/dice.go
@@ -1,26 +1,29 @@
+// 주사위 시뮬레이터
 package main
 
-import(
+import (
 	"fmt"
 	"math/rand/v2"
 )
-func dicing(n int) []int{
-	dice := map[int]int{}
-		
-	for i:=0 ; i < n ; i++ {
-		dice[rand.IntN(6)+1]++
+
+func dicing(n int) []int {
+	dice := make([]int, 6)
+
+	for i := 0; i < n; i++ {
+		dice[rand.IntN(6)]++
 	}
+	return dice
 }
-func main(){
+func main() {
 	var usr int
 	fmt.Println("주사위 시뮬레이터")
-	fmt.Println("몇번을 주사위 돌려볼까요? ")
+	fmt.Println("횟수 : ")
 
 	fmt.Scanf("%d\n", &usr)
 
 	a := dicing(usr)
-	fmt.Println("결과 : ")
-	for k,v:=range a{
-		fmt.Printf("[%d] : %d", k,v)	
+	fmt.Println("--> 결과 (기대값 : 0.1666)")
+	for k, v := range a {
+		fmt.Printf("[%d] : %d/%d (%.2f)\n", k+1, v, usr, float64(v)/float64(usr))
 	}
 }
